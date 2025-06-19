292522bc822e5b87df5bfcbc640f55372718a34c
Koji Eguchi <koji.eguchi@hotmail.com>
2025-06-18 21:20:46 -0400

    Add login page

--- a_core/settings.py
+++ a_core/settings.py
@@ -131,3 +131,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_EMAIL_REQUIRED = True
