1fe25fde95400e4f479307b29a5cd58c50231d1c
Koji Eguchi <koji.eguchi@hotmail.com>
2025-07-22 22:10:19 -0400

    Install and connect Websocket

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -7,10 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+from a_rtchat import routing
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "a_core.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
+})
--- a_core/settings.py
+++ a_core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -82,7 +83,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "a_core.wsgi.application"
+# WSGI_APPLICATION = "a_core.wsgi.application"
+
+ASGI_APPLICATION = "a_core.asgi.application"
 
 
 # Database
